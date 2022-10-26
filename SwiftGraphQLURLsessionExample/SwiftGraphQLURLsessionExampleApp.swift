35ebee6693a4940265c4712ac45b22d8b306758b
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-26 12:49:00 +0200

    Initial Commit

---
+++ SwiftGraphQLURLsessionExample/SwiftGraphQLURLsessionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGraphQLURLsessionExampleApp.swift
+//  SwiftGraphQLURLsessionExample
+//
+//  Created by Kaori Persson on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGraphQLURLsessionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
